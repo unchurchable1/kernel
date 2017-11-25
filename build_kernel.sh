092bf25b4a808e0dc1495548361928f92ac56a06
Jason Toney <jason.toney.0598@mail.linnbenton.edu>
2017-11-25 00:56:51 -0800

    first go at lean kernel
    BUGS:
    - touchpad
    - audio
    - ?

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+REVISION="0.1"
 VERSION="4.13"
 TARGET="linux-source-$VERSION"
 
@@ -44,16 +45,42 @@ clean_kernel() {
 }
 
 config_kernel() {
-	cat /boot/config-$(uname -r) \
-		| sed 's|# CONFIG_DRM_VBOXVIDEO is not set|CONFIG_DRM_VBOXVIDEO=m|' \
+	cat arch/x86/configs/x86_64_defconfig \
+		| sed 's|CONFIG_LOGO=y|# CONFIG_LOGO is not set|' \
 		> .config
+
+	echo 'CONFIG_ASUS_LAPTOP=m' >> .config
+	echo 'CONFIG_ASUS_NB_WMI=m' >> .config
+	echo 'CONFIG_ASUS_WIRELESS=m' >> .config
+	echo 'CONFIG_ASUS_WMI=m' >> .config
+	echo 'CONFIG_ATH9K=m' >> .config
+	echo 'CONFIG_INPUT_JOYDEV=m' >> .config
+	echo 'CONFIG_INT340X_THERMAL=m' >> .config
+	echo 'CONFIG_INT3406_THERMAL=m' >> .config
+	echo 'CONFIG_INTEL_RAPL=m' >> .config
+	echo 'CONFIG_ITCO_WDT=m' >> .config
+	echo 'CONFIG_MOUSE_ELAN_I2C=m' >> .config
+	echo 'CONFIG_SND_HDA_CODEC_HDMI=m' >> .config
+	echo 'CONFIG_SND_HDA_CODEC_REALTEK=m' >> .config
+	echo 'CONFIG_SND_HDA_GENERIC=m' >> .config
+	echo 'CONFIG_SND_SOC=m' >> .config
+	echo 'CONFIG_SND_SOC_INTEL_SST=m' >> .config
+	echo 'CONFIG_SND_SOC_INTEL_SST_ACPI=m' >> .config
+	echo 'CONFIG_SND_SOC_INTEL_SST_MATCH=m' >> .config
+	echo 'CONFIG_SND_SST_ATOM_HIFI2_PLATFORM=m' >> .config
+	echo 'CONFIG_USB_XHCI_HCD=m' >> .config
+
+	echo 'CONFIG_ITCO_VENDOR_SUPPORT=y' >> .config
+	echo 'CONFIG_SND_HDA_PREALLOC_SIZE=2048' >> .config
+	echo 'CONFIG_SND_SOC_COMPRESS=y' >> .config
+
 	make olddefconfig
 }
 
 make_kernel() {
 	make-kpkg --initrd \
 		--jobs=$(cat /proc/cpuinfo | grep ^processor | wc -l) \
-		--revision=$VERSION.vbox \
+		--revision=$REVISION.$SUDO_USER \
 		kernel_image
 }
 
